37beed294946176844aae7f8de58558af2e0c2f4
alok182011 <alokkola21@gmail.com>
2026-02-10 11:55:40 +0530

    contact update

--- src/components/layout/component/MobileFooter.jsx
+++ src/components/layout/component/MobileFooter.jsx
@@ -6,18 +6,18 @@ export default function MobileFooter() {
       <div className="mobile-footer px-20 py-20 border-top-light js-mobile-footer">
         <div className="mobile-footer__number">
           <div className="text-17 fw-500 text-dark-1">Call us</div>
-          <div className="text-17 fw-500 text-purple-1">+91 1800 XXX XXXX</div>
+          <div className="text-17 fw-500 text-purple-1">8878868600, 8878868699</div>
         </div>
 
         <div className="lh-2 mt-10">
           <div>
-            Sample Papers for Class 10, 11 & 12
-            <br /> India
+            Sample Papers for Class 8 to 12
+            <br /> EduNoble Learning, Nipaniya, Indore (M.P.)
           </div>
-          <div>contact@edunoble.com</div>
+          <div>edunoble.learning@gmail.com</div>
         </div>
 
-        <div className="mobile-socials mt-10">
+        {/* <div className="mobile-socials mt-10">
           <Link
             to="#"
             className="d-flex items-center justify-center rounded-full size-40"
@@ -45,7 +45,7 @@ export default function MobileFooter() {
           >
             <i className="fa fa-linkedin"></i>
           </Link>
-        </div>
+        </div> */}
       </div>
     </>
   );
